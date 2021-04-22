f08de52ba231c2b4bd00a415736c8657f74874e3
Sebastian <57314609+muscarina@users.noreply.github.com>
2021-04-22 09:44:20 +0200

    Halved DEFAULT_AXIS_STEPS_PER_UNIT (to match 32 uStep)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -738,7 +738,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 1600, 1852 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 800, 926 }
 
 /**
  * Default Max Feed Rate (mm/s)
